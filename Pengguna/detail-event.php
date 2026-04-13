bc64e062dad54c88d13c3e0f7029fdf0ff93dcd7
gloriousren <farentresandra@gmail.com>
2026-04-13 17:49:47 +0700

    up

--- Pengguna/detail-event.php
+++ Pengguna/detail-event.php
@@ -13,6 +13,10 @@ $isLoggedIn = isset($_SESSION['user']);
 <title>EventSpeak - Detail Event</title>
 <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&amp;family=Inter:wght@300;400;500;600&amp;display=swap" rel="stylesheet"/>
 <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&amp;display=swap" rel="stylesheet"/>
+<link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    >
 <script src="https://cdn.tailwindcss.com?plugins=forms,container-queries"></script>
 <script id="tailwind-config">
         tailwind.config = {
